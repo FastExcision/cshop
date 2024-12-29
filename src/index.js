f6769be9ac31abe161c3ee6c5668962006055d7d
Fastexcision <atlee@yandex.ru>
2024-12-29 07:57:52 +0300

    try use script/bug appendchild bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.scss';
 import './index.html';
-import {sum, mult} from './modules/calc'; 
 import {scrollLeft, scrollRight} from './modules/slider-scroll';
 import './modules/timer';
 import './modules/modal-menu';
 import './modules/topScroll';
+import './modules/cards';
--- src/modules/calc.js
+++
@@ -1,4 +0,0 @@
-export const sum = (a, b) => a + b;
-export const minus = (a, b) => a + b;
-export const mult = (a, b) => a * b; 
-//пример экспорта функций
