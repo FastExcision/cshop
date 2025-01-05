00fda0ff9e74bcfcddac4533776a6f2638254d79
Fastexcision <atlee@yandex.ru>
2025-01-05 08:08:03 +0300

    work cards modal

--- src/modules/cards.js
+++ src/modules/cards.js
@@ -2,11 +2,80 @@ import cards from "../gifts.json";
 import forWorkImg from "../img/gift-for-work.png";
 import forHealthImg from "../img/gift-for-health.png";
 import forHarmImg from "../img/gift-for-harmony.png";
+import snowFill from "../img/snow-fill.svg"
+import snowUnfill from "../img/snow-unfill.svg"
+import closeButtonImg from "../img/State=Close.svg"
 
+const html = document.getElementsByTagName("html")[0];
 const giftCont = document.getElementById("best-gifts-container")
+const modalContainer = document.getElementById("cards-container-modal");
+const popover = document.getElementById("popover");
 
+const createCardModal = (element) => {
+    toggleDisplay()
+    const imgModal = document.createElement("img");
+    const closeButtonModal = document.createElement("img");
+    const cardContainerDescriptionModal = document.createElement("div");
+    imgModal.src = loadImg(element).src;
+    closeButtonModal.src = closeButtonImg;
+    closeButtonModal.classList.add("close-button-modal");
+    cardContainerDescriptionModal.classList.add("cards-container-modal_description-container");
+    modalContainer.appendChild(closeButtonModal);
+    modalContainer.appendChild(imgModal);
+    modalContainer.appendChild(cardContainerDescriptionModal);
+    firstChild(element, cardContainerDescriptionModal);
+    closeButtonModal.addEventListener("click", toggleDisplay);
+}
 
+const firstChild = (element, cardContainerDescriptionModal) => {
+    const titleCategory = document.createElement("h4");
+    const titleName = document.createElement("h3");
+    const description = document.createElement("p");
+    const titleSuperpowers = document.createElement("h4");
+    const containerSuperpowers = document.createElement("div");
+    cardContainerDescriptionModal.appendChild(titleCategory);
+    cardContainerDescriptionModal.appendChild(titleName);
+    cardContainerDescriptionModal.appendChild(description);
+    cardContainerDescriptionModal.appendChild(titleSuperpowers);
+    cardContainerDescriptionModal.appendChild(containerSuperpowers);
+    containerSuperpowers.classList.add("SUPERPOWERS-container-modal");
+    categoryInner(element, titleCategory);
+    titleName.textContent = element.name;
+    description.textContent = element.description;
+    titleSuperpowers.textContent = "ADDS SUPERPOWERS TO:";
+    superpowersContent(element, containerSuperpowers);
+}
 
+const categoryInner = (element, titleCategory) => {
+    titleCategory.textContent = element.category;
+    if (element.category === "For Work") {
+        titleCategory.classList.add("categories-for-work");
+    } else if (element.category === "For Health") {
+        titleCategory.classList.add("categories-for-health");
+    } else titleCategory.classList.add("categories-for-harmony");
+}
+
+const superpowersContent = (element, containerSuperpowers) => {
+    const superpowersName = ["Live","Create","Love","Dream"];
+    const superpowerContentArr = [element.superpowers.live, element.superpowers.create, element.superpowers.love, element.superpowers.dream]
+    for (let i = 0; i < superpowersName.length; i++) {
+        const superpower = document.createElement("p");
+        const superpowerContent = document.createElement("p");
+        const superpowerIconContainer = document.createElement("div");
+        superpowerIconContainer.classList.add("superpower-icon-container");
+        containerSuperpowers.appendChild(superpower);
+        containerSuperpowers.appendChild(superpowerContent);
+        containerSuperpowers.appendChild(superpowerIconContainer);
+        superpower.textContent = superpowersName[i];
+        superpowerContent.textContent = superpowerContentArr[i];
+        for (let l = 0; l < (superpowerContentArr[i]/100); l++) {
+            superpowerIconContainer.appendChild(document.createElement("img")).src = snowFill;
+        }
+        for (let l = (superpowerContentArr[i]/100); l < 5; l++) {
+            superpowerIconContainer.appendChild(document.createElement("img")).src = snowUnfill;
+        }
+    }
+}
 
 const loadDescription = (element) => {
     const DescCont = document.createElement("div");
@@ -33,7 +102,7 @@ const loadCards = (element) => {
     giftCont.appendChild(cardContainer);
     cardContainer.appendChild(loadImg(element));
     cardContainer.appendChild(loadDescription(element));
-    // cardContainer.addEventListener("click",() => createCardModal(element));
+    cardContainer.addEventListener("click",() => createCardModal(element));
 }
 
 const start = (qty) => {
@@ -49,6 +118,14 @@ const start = (qty) => {
     }
 }
 
+const toggleDisplay = () => {
+    html.classList.toggle("disable-scroll");
+    modalContainer.classList.toggle("display-none");
+    popover.classList.toggle("display-none");
+    modalContainer.replaceChildren()
+}
+
+
 start(4);
 
-
+popover.addEventListener("click", toggleDisplay);
--- src/modules/modal-cards.js
+++ src/modules/modal-cards.js
@@ -1,84 +1,88 @@
-import snowFill from "../img/snow-fill.svg"
-import snowUnfill from "../img/snow-unfill.svg"
-
-const modalContainer = document.getElementById("cards-container-modal");
-
-const createCardModal = (element) => {
-    const imgModal = document.createElement("img");
-    const cardContainerDescriptionModal = document.createElement("div");
-    imgModal.src = loadImg(element).src;
-    modalContainer.appendChild(cardContainerDescriptionModal);
-    modalContainer.appendChild(imgModal);
-    cardContainerDescriptionModal.classList.add("cards-container-modal_description-container");
-    cardContainerDescriptionModal.appendChild(firstChild(element));
-}
-
-const firstChild = (element) => {
-    const titleCategory = document.createElement("h4");
-    const titleName = document.createElement("h3");
-    const description = document.createElement("p");
-    const titleSuperpowers = document.createElement("h3");
-    const containerSuperpowers = document.createElement("div");
-    containerSuperpowers.classList.add("SUPERPOWERS-container-modal");
-    categoryInner(element, titleCategory);
-    titleName.textContent = element.name;
-    description.textContent = element.description;
-    titleSuperpowers.textContent = "ADDS SUPERPOWERS TO:";
-    superpowersContent(element, containerSuperpowers);
-}
-
-const categoryInner = (element, titleCategory) => {
-    titleCategory.textContent = element.category;
-    if (element.category === "For Work") {
-        titleCategory.classList.add("cards-for-work");
-    } else if (element.category === "For Health") {
-        titleCategory.classList.add("cards-for-health");
-    } titleCategory.classList.add("cards-for-harmony");
-}
-
-const superpowersContent = (element,containerSuperpowers) => {
-    const superpowersName = ["Live","Create","Love","Dream"];
-    const superpowerContentArr = [element.superpowers.live, element.superpowers.create, element.superpowers.love, element.superpowers.dream]
-    for (let i = 0; i < superpowersName.length; i++) {
-        const superpower = document.createElement("p");
-        const superpowerContent = document.createElement("p");
-        const superpowerIconContainer = document.createElement("div");
-        containerSuperpowers.appendChild(superpower);
-        containerSuperpowers.appendChild(superpowerContent);
-        containerSuperpowers.appendChild(superpowerIconContainer);
-        superpower.textContent = superpowersName[i];
-        superpowerContent.textContent = superpowerContentArr[i];
-        for (let i = 0; i < (superpowerContentArr[i]/100); i++) {
-            superpowerIconContainer.appendChild(document.createElement("img").src = snowFill);
-        }
-        for (let i = 5 - superpowerContentArr[i]/100; i < 5; i++) {
-            superpowerIconContainer.appendChild(document.createElement("img").src = snowUnfill);
-        }
-    }
-}
-
-//     <div className="SUPERPOWERS-container-modal">
-//         <p>Live </p><p></p>
-//         <div id="modal-superpower-live_icon"></div>
-//         <p>Create</p><p></p>
-//         <div id="modal-superpower-create_icon"></div>
-//         <p>Love</p><p></p>
-//         <div id="modal-superpower-love_icon"></div>
-//         <p>Dream</p><p></p>
-//         <div id="modal-superpower-dream_icon"></div>
-//     </div>
-
-
-// <img className="card-image" alt="card image" id="modal-cards-image"/>
-// <div className="cards-container-modal_description-container">
-//     <h4></h4>
-//     <h3></h3>
-//     <p></p>
-//     <h3>ADDS SUPERPOWERS TO:</h3>
-
-// </div>
-
-const toggleDisplay = () => {
-    cardModal.classList.toggle("display-none");
-    popover.classList.toggle("display-none");
-}
+// import snowFill from "../img/snow-fill.svg"
+// import snowUnfill from "../img/snow-unfill.svg"
+//
+// const modalContainer = document.getElementById("cards-container-modal");
+//
+// const createCardModal = (element) => {
+//     const imgModal = document.createElement("img");
+//     const cardContainerDescriptionModal = document.createElement("div");
+//     imgModal.src = loadImg(element).src;
+//     cardContainerDescriptionModal.classList.add("cards-container-modal_description-container");
+//     modalContainer.appendChild(imgModal);
+//     modalContainer.appendChild(cardContainerDescriptionModal);
+//     firstChild(element, cardContainerDescriptionModal);
+// }
+//
+// const firstChild = (element, cardContainerDescriptionModal) => {
+//     const titleCategory = document.createElement("h4");
+//     const titleName = document.createElement("h3");
+//     const description = document.createElement("p");
+//     const titleSuperpowers = document.createElement("h3");
+//     const containerSuperpowers = document.createElement("div");
+//     cardContainerDescriptionModal.appendChild(titleCategory);
+//     cardContainerDescriptionModal.appendChild(titleName);
+//     cardContainerDescriptionModal.appendChild(description);
+//     cardContainerDescriptionModal.appendChild(titleSuperpowers);
+//     cardContainerDescriptionModal.appendChild(containerSuperpowers);
+//     containerSuperpowers.classList.add("SUPERPOWERS-container-modal");
+//     categoryInner(element, titleCategory);
+//     titleName.textContent = element.name;
+//     description.textContent = element.description;
+//     titleSuperpowers.textContent = "ADDS SUPERPOWERS TO:";
+//     superpowersContent(element, containerSuperpowers);
+// }
+//
+// const categoryInner = (element, titleCategory) => {
+//     titleCategory.textContent = element.category;
+//     if (element.category === "For Work") {
+//         titleCategory.classList.add("categories-for-work");
+//     } else if (element.category === "For Health") {
+//         titleCategory.classList.add("categories-for-health");
+//     } titleCategory.classList.add("categories-for-harmony");
+// }
+//
+// const superpowersContent = (element, containerSuperpowers) => {
+//     const superpowersName = ["Live","Create","Love","Dream"];
+//     const superpowerContentArr = [element.superpowers.live, element.superpowers.create, element.superpowers.love, element.superpowers.dream]
+//     for (let i = 0; i < superpowersName.length; i++) {
+//         const superpower = document.createElement("p");
+//         const superpowerContent = document.createElement("p");
+//         const superpowerIconContainer = document.createElement("div");
+//         containerSuperpowers.appendChild(superpower);
+//         containerSuperpowers.appendChild(superpowerContent);
+//         containerSuperpowers.appendChild(superpowerIconContainer);
+//         superpower.textContent = superpowersName[i];
+//         superpowerContent.textContent = superpowerContentArr[i];
+//         for (let l = 0; l < (superpowerContentArr[i]/100); l++) {
+//             superpowerIconContainer.appendChild(document.createElement("img")).src = snowFill;
+//         }
+//         for (let l = (superpowerContentArr[i]/100); l < 5; l++) {
+//             superpowerIconContainer.appendChild(document.createElement("img")).src = snowUnfill;
+//         }
+//     }
+// }
+// //     <div className="SUPERPOWERS-container-modal">
+// //         <p>Live </p><p></p>
+// //         <div id="modal-superpower-live_icon"></div>
+// //         <p>Create</p><p></p>
+// //         <div id="modal-superpower-create_icon"></div>
+// //         <p>Love</p><p></p>
+// //         <div id="modal-superpower-love_icon"></div>
+// //         <p>Dream</p><p></p>
+// //         <div id="modal-superpower-dream_icon"></div>
+// //     </div>
+//
+//
+// // <img className="card-image" alt="card image" id="modal-cards-image"/>
+// // <div className="cards-container-modal_description-container">
+// //     <h4></h4>
+// //     <h3></h3>
+// //     <p></p>
+// //     <h3>ADDS SUPERPOWERS TO:</h3>
+//
+// // </div>
+//
+// const toggleDisplay = () => {
+//     cardModal.classList.toggle("display-none");
+//     popover.classList.toggle("display-none");
+// }
